6e01c88ea2ffefaee426bb031d921b27d51de4d6
Myrmex <dfusi@hotmail.com>
2022-02-12 18:53:35 +0100

    GrfLocationPart

---
+++ Cadmus.Tivac.Parts.Test/GrfLocationPartTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Xunit;
+using Cadmus.Core;
+using System.Collections.Generic;
+using System.Linq;
+using Cadmus.Seed.Tivac.Parts;
+
+namespace Cadmus.Tivac.Parts.Test
+{
+    public sealed class GrfLocationPartTest
+    {
+        private static GrfLocationPart GetPart()
+        {
+            GrfLocationPartSeeder seeder = new();
+            IItem item = new Item
+            {
+                FacetId = "default",
+                CreatorId = "zeus",
+                UserId = "zeus",
+                Description = "Test item",
+                Title = "Test Item",
+                SortKey = ""
+            };
+            return (GrfLocationPart)seeder.GetPart(item, null, null);
+        }
+
+        private static GrfLocationPart GetEmptyPart()
+        {
+            return new GrfLocationPart
+            {
+                ItemId = Guid.NewGuid().ToString(),
+                RoleId = "some-role",
+                CreatorId = "zeus",
+                UserId = "another",
+            };
+        }
+
+        [Fact]
+        public void Part_Is_Serializable()
+        {
+            GrfLocationPart part = GetPart();
+
+            string json = TestHelper.SerializePart(part);
+            GrfLocationPart part2 = TestHelper.DeserializePart<GrfLocationPart>(json)!;
+
+            Assert.Equal(part.Id, part2.Id);
+            Assert.Equal(part.TypeId, part2.TypeId);
+            Assert.Equal(part.ItemId, part2.ItemId);
+            Assert.Equal(part.RoleId, part2.RoleId);
+            Assert.Equal(part.CreatorId, part2.CreatorId);
+            Assert.Equal(part.UserId, part2.UserId);
+        }
+
+        [Fact]
+        public void GetDataPins_Tag_1()
+        {
+            GrfLocationPart part = GetEmptyPart();
+            part.Place = "place1";
+            part.Area = "area1";
+            part.District = "district1";
+
+            List<DataPin> pins = part.GetDataPins(null).ToList();
+            Assert.Equal(3, pins.Count);
+
+            DataPin? pin = pins.Find(p => p.Name == "place" && p.Value == "place1");
+            Assert.NotNull(pin);
+            TestHelper.AssertPinIds(part, pin!);
+
+            pin = pins.Find(p => p.Name == "area" && p.Value == "area1");
+            Assert.NotNull(pin);
+            TestHelper.AssertPinIds(part, pin!);
+
+            pin = pins.Find(p => p.Name == "district" && p.Value == "district1");
+            Assert.NotNull(pin);
+            TestHelper.AssertPinIds(part, pin!);
+        }
+    }
+}
---
+++ Cadmus.Tivac.Parts.Test/TestHelper.cs
@@ -0,0 +1,40 @@
+﻿using Cadmus.Core;
+using System;
+using System.Text.Json;
+using Xunit;
+
+namespace Cadmus.Tivac.Parts.Test
+{
+    internal static class TestHelper
+    {
+        private static readonly JsonSerializerOptions _options =
+            new()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+
+        public static string SerializePart(IPart part)
+        {
+            if (part == null)
+                throw new ArgumentNullException(nameof(part));
+
+            return JsonSerializer.Serialize(part, part.GetType(), _options);
+        }
+
+        public static T? DeserializePart<T>(string json)
+            where T : class, IPart, new()
+        {
+            if (json == null)
+                throw new ArgumentNullException(nameof(json));
+
+            return JsonSerializer.Deserialize<T>(json, _options);
+        }
+
+        public static void AssertPinIds(IPart part, DataPin pin)
+        {
+            Assert.Equal(part.ItemId, pin.ItemId);
+            Assert.Equal(part.Id, pin.PartId);
+            Assert.Equal(part.RoleId, pin.RoleId);
+        }
+    }
+}
